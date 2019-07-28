36ea73c3cf5993bfa46ecc98c05a62fbf63ecec2
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 22:54:05 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
 end
